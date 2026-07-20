969adbd3a051baba7e11d2eb524d482b5857934b
Sekvojak <dominikkontrik123@gmail.com>
2026-07-20 10:57:47 +0200

    initial backend project setup

---
+++ backend/src/main/java/com/sekvojak/trailplanner/TrailplannerApplication.java
@@ -0,0 +1,13 @@
+package com.sekvojak.trailplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailplannerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sekvojak/trailplanner/TrailplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sekvojak.trailplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
